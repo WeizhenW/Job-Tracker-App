86311d0719cca91a31d38aa63320ff952143ec9f
Weizhen Wang <wangwzhen@gmail.com>
2019-06-30 17:50:51 -0500

    create input job page UI

--- src/components/InputJob/InputJob.js
+++ src/components/InputJob/InputJob.js
@@ -5,9 +5,29 @@ class InputJob extends Component {
     render() {
         return (
             <div>
-                <p>
-                    Input Job Page
-                </p>
+                <h2>Enter a new InputJob</h2>
+                <form>
+                    Company Name:
+                    <input type="text" name="companyName" /><br />
+                    Job Title:
+                    <input type="text" name="jobTitle" /><br />
+                    Post URL:
+                    <input type="text" name="postUrl" /><br />
+                    Status:
+                    <select type="text" name="status">
+                        <option value="new">New</option>
+                        <option value="applied">Applied</option>
+                        <option value="phone">Phone Interview</option>
+                        <option value="onsite">Onsite Interview</option>
+                        <option value="reject">Reject</option>
+                        <option value="offer">Offer</option>
+                    </select>
+                    
+                    <br />
+                    <br />
+                    <input type="submit" value="Cancel" />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         )
     }
