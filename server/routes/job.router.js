6a279d0e1155444a8726dcd656408601c1104699
Weizhen Wang <wangwzhen@gmail.com>
2019-06-30 21:42:21 -0500

    include user id in the post new job route

--- server/routes/job.router.js
+++ server/routes/job.router.js
@@ -6,8 +6,9 @@ const router = express.Router();
 router.post('/new', (req, res) => {
     // console.log(req.body);
     const newJob = req.body;
-    pool.query(`INSERT INTO "job" ("title", "post_url", "company", "status_id")
-    VALUES($1, $2, $3, $4);`, [newJob.jobTitle, newJob.postUrl, newJob.companyName, newJob.status_id])
+    console.log('req.user', req.user);
+    pool.query(`INSERT INTO "job" ("title", "post_url", "company", "status_id", "user_id")
+    VALUES($1, $2, $3, $4, $5);`, [newJob.jobTitle, newJob.postUrl, newJob.companyName, newJob.status_id, req.user.id])
     .then(() => res.sendStatus(200))
     .catch(error => {
         console.log('error with post new job', error);
@@ -21,6 +22,16 @@ router.get('/toapply', (req, res) => {
     .then((result) => res.send(result.rows))
     .catch(error => {
         console.log('error with get new jobs list', error);
+        res.sendStatus(500);
+    })
+})
+
+//route to delete one job
+router.delete('/delete', (req, res) => {
+    pool.query(`DELETE FROM "job" WHERE "id"=$1;` [req.body.id])
+    .then(() => res.sendStatus(200))
+    .catch(error => {
+        console.log('error with delete job', error);
         res.sendStatus(500);
     })
 })
