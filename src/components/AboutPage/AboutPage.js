5f446d848116db7e56a5abc8098075f82e14e1f4
Weizhen Wang <wangwzhen@gmail.com>
2019-07-12 11:27:41 -0500

    add  footer and about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './AboutPage.css';
+import '../Techniques/Techniques';
+import Techniques from '../Techniques/Techniques';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
@@ -7,11 +10,21 @@ import React from 'react';
 
 const AboutPage = () => (
   <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
-    </div>
+  <div className="aboutInfo">
+      <p>Job Cracker is an app there will facilitate the job application process for the job seekers, by centralizing the job hunting related information in a single location.</p> 
+      <p>User will be able to:</p>
+      <ul>
+        <li>Add a new job to the job list, delete an existing job from the list, and update job status along with the application process</li>
+        <li>Browse and update job details</li>
+        <li>Upload the job related files into AWS S3 and retrieve them when needed</li>
+        <li>Fetch new job posts from Indeed.com and add a job from indeed to the job list by a single click</li>
+        <li>Get reminders for follow up actions after application or interview</li>
+        <li>Search for jobs by company name or by status in the job list</li>
+        <li>Add connections into the directory, and add a contact as referee to a job</li>
+        <li>Track job application progress from the dashboard</li>
+      </ul>
+  </div>
+  <Techniques />
   </div>
 );
 
